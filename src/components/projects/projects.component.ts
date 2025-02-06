e2a52c45544ea634f4288d85d04592133805217e
Akram <akrambenaoun1993@gmail.com>
2025-02-06 23:53:50 +0100

    projects

--- src/components/projects/projects.component.ts
+++ src/components/projects/projects.component.ts
@@ -1,31 +1,24 @@
 import { Component } from '@angular/core';
+import { SafeUrlPipe } from './safe-url.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone:true,
-  imports:[],
+  imports:[SafeUrlPipe, CommonModule],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class projectsComponent {
+export class ProjectsComponent {
+  files = [
+    { path: 'assets/Complete web dev Certificate.pdf', type: 'pdf', name: 'Complete web dev Certificate' },
+    { path: 'assets/Docker Beginner Certificate.pdf', type: 'pdf', name: 'Docker Beginner Certificate' },
+    { path: 'assets/Kubernetes Beginner Certificate.pdf', type: 'pdf', name: 'Kubernetes Beginner Certificate' },
+    { path: 'assets/RBK Certificate.jpeg', type: 'image', name: 'RBK Certificate' },
+    { path: 'assets/Scrum Study Certificate.jpeg', type: 'image', name: 'Scrum Study Certificate' }
+  ];
 
-  scrollLeft() {
-    const container = document.querySelector('.scroll-container');
-    if (container) {
-      container.scrollBy({
-        left: -300,
-        behavior: 'smooth'
-      });
-    }
-  }
-
-  scrollRight() {
-    const container = document.querySelector('.scroll-container');
-    if (container) {
-      container.scrollBy({
-        left: 300,
-        behavior: 'smooth'
-      });
-    }
+  openFullscreen(path: string) {
+    window.open(path, '_blank');
   }
 }
