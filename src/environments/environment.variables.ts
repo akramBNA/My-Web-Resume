183542c0c471751012d4edde31ead2a6c9fc11da
Akram <akrambenaoun1993@gmail.com>
2025-03-15 18:26:08 +0100

    redo environment ignore changes! to be tried again later

---
+++ src/environments/environment.variables.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  EMAILJS_SERVICE_ID: 'service_2trf6u1',
+  EMAILJS_TEMPLATE_ID: 'template_b9dfigf',
+  EMAILJS_AUTO_REPLY_TEMPLATE_ID: 'template_rt17whj',
+  EMAILJS_PUBLIC_KEY: '1PX2P6dWBFkvJm3qZ',
+};
