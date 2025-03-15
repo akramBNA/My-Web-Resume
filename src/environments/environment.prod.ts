c6cb8f38a8e36ca0e090d3e8851c0fb910b6a177
Akram <akrambenaoun1993@gmail.com>
2025-03-15 17:58:22 +0100

    Removed environment files from Git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { Environment } from './environment.interface';
-
-export const environment: Environment = {
-  production: true,
-  EMAILJS_SERVICE_ID: 'service_2trf6u1',
-  EMAILJS_TEMPLATE_ID: 'template_b9dfigf',
-  EMAILJS_AUTO_REPLY_TEMPLATE_ID: 'template_rt17whj',
-  EMAILJS_PUBLIC_KEY: '1PX2P6dWBFkvJm3qZ',
-};
