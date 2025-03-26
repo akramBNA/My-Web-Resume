2461265bb42e1a50d1c5de77c476327a7e019157
Akram <akrambenaoun1993@gmail.com>
2025-03-26 15:46:46 +0100

    updates

---
+++ src/environments/environment.variables.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  production: false,
+  EMAILJS_SERVICE_ID: 'service_2trf6u1',
+  EMAILJS_TEMPLATE_ID: 'template_b9dfigf',
+  EMAILJS_AUTO_REPLY_TEMPLATE_ID: 'template_rt17whj',
+  EMAILJS_PUBLIC_KEY: '1PX2P6dWBFkvJm3qZ',
+  GOOGLE_ANALYTICS_ID: 'G-MZYDMBW5VN',
+};
