a7de60badbb06fa9a8d7b1b6d307cc2424cc0381
Akram <akrambenaoun1993@gmail.com>
2026-04-02 16:52:09 +0100

    add a new resumes route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ResumeMainComponent } from '../components/resume/resume-main/resume-main.component';
 
 export const routes: Routes = [
   { path: '', component: AppComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
+  { path: 'resume', loadComponent: () => import('../components/resume/resume-main/resume-main.component').then(m => m.ResumeMainComponent) }
 ];
