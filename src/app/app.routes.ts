048eb1f2057fef4d1756ba399093cd654f953bfd
Akram <akrambenaoun1993@gmail.com>
2026-04-03 18:04:00 +0100

    Remove lazy loaded home component!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { ResumeMainComponent } from '../components/resume/resume-main/resume-main.component';
+import { HomeComponent } from '../components/home page/home/home.component';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('../components/home page/home/home.component').then(m => m.HomeComponent) },
+  { path: '', component: HomeComponent },
   { path: 'resume', loadComponent: () => import('../components/resume/resume-main/resume-main.component').then(m => m.ResumeMainComponent) },
   { path: '**', redirectTo: '' },
 ];
